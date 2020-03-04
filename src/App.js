3cd6ac60e7fbcf09fc54484f6b811cc551e93c95
hitzu <hitzu1001@gmail.com>
2020-03-04 12:03:00 +1100

    Add Color Helper to Generate Colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
-
+import { generatePalette } from './colorHelpers'
 
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div>
-      <Palette {...seedColors[2]}/>
+      <Palette {...seedColors[2]} />
     </div>
   );
 }
